12def5de6a9f672757d99a586d9cb241806a0a6e
otubby <48339163+otubby@users.noreply.github.com>
2021-12-11 15:06:25 -0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\mizuk\\OneDrive\\Documents\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\mizuk\\OneDrive\\Documents\\lifecounter" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\mizuk\\OneDrive\\Documents\\flutter"
+  "PROJECT_DIR=C:\\Users\\mizuk\\OneDrive\\Documents\\lifecounter"
+  "FLUTTER_ROOT=C:\\Users\\mizuk\\OneDrive\\Documents\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\mizuk\\OneDrive\\Documents\\lifecounter\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\mizuk\\OneDrive\\Documents\\lifecounter"
+  "FLUTTER_TARGET=C:\\Users\\mizuk\\OneDrive\\Documents\\lifecounter\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\mizuk\\OneDrive\\Documents\\lifecounter\\.dart_tool\\package_config.json"
+)
